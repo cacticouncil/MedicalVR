78a05e978192588af2896f2b8ebd8ad83ddbc14e
Jordan Sanderson <JTSanderson@fullsail.edu>
2017-02-02 13:47:31 -0500

    Made a base level

---
+++ Medical VR/Assets/MoveCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+    public float speed;
+    private float startTime;
+    private float distance;
+    private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+        distance = Vector3.Distance(transform.position, position);
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float disCovered = (Time.time - startTime) * speed;
+        float fracJourney = disCovered / distance;
+        transform.position = Vector3.Lerp(transform.position, position, fracJourney);
+	}
+
+    public void SetDestination(Vector3 endpoint)
+    {
+        position = new Vector3(endpoint.x, transform.position.y, endpoint.z);
+        distance = Vector3.Distance(transform.position, position);
+        startTime = Time.time;
+    }
+}
---
+++ Medical VR/Assets/MoveCharacter.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class MoveCharacter : MonoBehaviour, IGvrGazeResponder
+{
+    private Vector3 startingPosition;
+    public MoveCamera mainCamera;
+
+    void Start()
+    {
+        startingPosition = transform.localPosition;
+        SetGazedAt(false);
+    }
+
+    void LateUpdate()
+    {
+        GvrViewer.Instance.UpdateState();
+        if (GvrViewer.Instance.BackButtonPressed)
+        {
+            Application.Quit();
+        }
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+    }
+
+    public void Reset()
+    {
+        transform.localPosition = startingPosition;
+    }
+
+    public void ToggleVRMode()
+    {
+        GvrViewer.Instance.VRModeEnabled = !GvrViewer.Instance.VRModeEnabled;
+    }
+
+    public void ToggleDistortionCorrection()
+    {
+        GvrViewer.Instance.DistortionCorrectionEnabled =
+          !GvrViewer.Instance.DistortionCorrectionEnabled;
+    }
+
+#if !UNITY_HAS_GOOGLEVR || UNITY_EDITOR
+    public void ToggleDirectRender()
+    {
+        GvrViewer.Controller.directRender = !GvrViewer.Controller.directRender;
+    }
+#endif  //  !UNITY_HAS_GOOGLEVR || UNITY_EDITOR
+
+    public void MoveChar()
+    {
+        mainCamera.SetDestination(transform.position);
+    }
+
+    #region IGvrGazeResponder implementation
+
+    //Called when the user is looking on a GameObject with this script,
+    // as long as it is set to an appropriate layer (see GvrGaze).
+    public void OnGazeEnter()
+    {
+        SetGazedAt(true);
+    }
+
+    //Called when the user stops looking on the GameObject, after OnGazeEnter
+    // was already called.
+    public void OnGazeExit()
+    {
+        SetGazedAt(false);
+    }
+
+    //Called when the viewer's trigger is used, between OnGazeEnter and OnGazeExit.
+    public void OnGazeTrigger()
+    {
+        MoveChar();
+    }
+
+    #endregion
+}
+
