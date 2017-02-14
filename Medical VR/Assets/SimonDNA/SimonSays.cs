64c1e2240796dddba124b8681e46918a2dc8d502
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-02-14 16:00:34 -0500

    Somin says minigame setup

---
+++ Medical VR/Assets/SimonDNA/SimonSays.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimonSays : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject yellow;
+    public GameObject red;
+    public GameObject blue;
+    public GameObject green; 
+
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
