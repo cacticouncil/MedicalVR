311de6310f6766f5b654578f5550aa178c010cf2
Jayson Levario <jjlevario@fullsail.edu>
2017-02-23 16:36:06 -0500

    added cell and virus movement

--- Medical VR/Assets/GoogleVR/Scripts/FightVirus/Bullet/BulletManager.cs
+++ Medical VR/Assets/GoogleVR/Scripts/FightVirus/Bullet/BulletManager.cs
@@ -26,6 +26,7 @@ public class BulletManager : MonoBehaviour
             StartShooting();
         }
 
+        //Checking for button input
         if (GvrViewer.Instance.Triggered)
         {
             isTriggered = !isTriggered;
