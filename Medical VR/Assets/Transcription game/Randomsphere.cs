52a4ec403e09829cb4de64906a6b5c2b38541f56
Sebastian <sebs_dragneel@hotmail.com>
2017-02-28 10:35:56 -0500

    memory game changes

--- Medical VR/Assets/Transcription game/Randomsphere.cs
+++ Medical VR/Assets/Transcription game/Randomsphere.cs
@@ -4,13 +4,33 @@ using System.Collections;
 public class Randomsphere : MonoBehaviour {
 
     public GameObject[]  Spheres = new GameObject[5];
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private int i = Random.Range(0, 4);
+    private int j = Random.Range(0, 4);
+    private int k = Random.Range(0, 4);
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(Spheres[i], new Vector3(-35,26,30), Spheres[0].transform.rotation);
+
+        while (j == 3 || j == i)
+            j = Random.Range(0, 4);
+
+
+                Instantiate(Spheres[j], new Vector3(-20, 26, 30), Spheres[1].transform.rotation);
+           
+        
+            while (k == 2 || k == j || k == 1)
+            k = Random.Range(0, 4);
+
+
+            Instantiate(Spheres[k], new Vector3(-5, 26, 30), Spheres[2].transform.rotation);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
