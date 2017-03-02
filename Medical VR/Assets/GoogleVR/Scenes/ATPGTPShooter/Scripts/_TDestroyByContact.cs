34e94f181023e9ca3f5a6b4597a179610965943d
Michael <Hammeric2000@yahoo.com>
2017-03-02 14:45:45 -0500

    Added the ATPGTPShooter game. Added temporary bullets Added temporary viruses made the bullets and viruses collide with each other and destroy each other.

---
+++ Medical VR/Assets/GoogleVR/Scenes/ATPGTPShooter/Scripts/_TDestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _TDestroyByContact : MonoBehaviour
+{
+    //public GameObject explosion;
+    public int scoreValue;
+    //private GameController gameController;
+
+    private void Start()
+    {
+        //GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        //if (gameControllerObject)
+        //{
+        //    gameController = gameControllerObject.GetComponent<GameController>();
+        //}
+        //if (!gameController)
+        //{
+        //    Debug.Log("Cannot find 'GameController' script");
+        //}
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        //Instantiate(explosion, transform.position, transform.rotation);
+
+        
+        //gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
