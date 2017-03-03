6fea60c662ee2f10cd4b8ab108e8affd49c6c7fd
Jordan Sanderson <JTSanderson@fullsail.edu>
2017-03-03 09:19:18 -0500

    strategy tutorial being made

---
+++ Medical VR/Assets/Strategy Game/StrategyTutorialD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StrategyTutorialD : MonoBehaviour
+{
+    public int defense = 1;
+    public int defCap = 5;
+    public TextMesh text;
+    public GameObject virus, cell;
+
+    public void Click()
+    {
+        if (defense < defCap)
+        {
+            defense++;
+            text.text = "Defense: " + defense;
+        }
+    }
+
+    
+}
---
+++ Medical VR/Assets/Strategy Game/StrategyTutorialR.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StrategyTutorialR : MonoBehaviour
+{
+    public int reproduction = 1;
+    public int repCap = 5;
+    public TextMesh text;
+    public GameObject left, right;
+
+    public void Click()
+    {
+        if (reproduction < repCap)
+        {
+            reproduction++;
+            text.text = "Reproduction: " + reproduction;
+            left.GetComponent<MoveLeftOrRight>().IncreaseSpeed();
+            right.GetComponent<MoveLeftOrRight>().IncreaseSpeed();
+        }
+    }
+}
