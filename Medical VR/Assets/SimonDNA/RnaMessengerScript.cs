b4e4b51bf5913393c9b35652ae8002b7e0f20ebe
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-03-08 14:00:26 -0500

    SimonDNA.unity rna messenger in progress

--- Medical VR/Assets/SimonDNA/RnaMessengerScript.cs
+++ Medical VR/Assets/SimonDNA/RnaMessengerScript.cs
@@ -6,27 +6,56 @@ public class RnaMessengerScript : MonoBehaviour {
 
     public GameObject Refpolynuclei, simon;
     List<GameObject> chain = new List<GameObject>();
+    List<GameObject> wrongChain = new List<GameObject>();
     List<Vector3> prevPos = new List<Vector3>();
-    int tmpScore = 0;
+    int wave = 0, tmpScore, tmpLives;
     Vector3 target;
+    bool changeMove = false;
     // Use this for initialization
     void Start ()
     {
-       tmpScore = simon.GetComponent<SimonSays>().score;
+        tmpScore = simon.GetComponent<SimonSays>().score;
+        tmpLives = simon.GetComponent<SimonSays>().lives;
+        changeMove = false;
     }
 	// Update is called once per frame
 	void Update ()
     {
-	    if(tmpScore!= simon.GetComponent<SimonSays>().score)
+
+        if (tmpLives != simon.GetComponent<SimonSays>().lives)
         {
-           tmpScore = simon.GetComponent<SimonSays>().score;
+            tmpLives = simon.GetComponent<SimonSays>().lives;
+            if (wave > 0)
+            {
+                RemoveNuclei();
+                changeMove = true;
+            }
+            simon.GetComponent<SimonSays>().makeNuclei = false;
+        }
+       else if (simon.GetComponent<SimonSays>().makeNuclei == true)
+        {
+            simon.GetComponent<SimonSays>().makeNuclei = false;
            AddNuclei();
+            changeMove = false;
         }
         BreakAnimation();
-        MoveNuclei();
+        if(changeMove == false)
+        {
+            MoveNuclei();
+        }
+        else
+        {
+            MoveBack();
+        }
+        if (tmpScore != simon.GetComponent<SimonSays>().score)
+        {
+            tmpScore = simon.GetComponent<SimonSays>().score;
+            wave = 0;
+        }
     }
     void AddNuclei()
     {
+        wave++;
         GameObject nuclei = Instantiate(Refpolynuclei, Refpolynuclei.transform.position, Refpolynuclei.transform.rotation) as GameObject;
         switch (simon.GetComponent<SimonSays>().selectedColor)
         {
@@ -53,6 +82,23 @@ public class RnaMessengerScript : MonoBehaviour {
             prevPos.Add(chain[i].transform.position);
         }
     }
+    void RemoveNuclei()
+    {
+        for (int i = chain.Count -1; i > chain.Count - 1 - wave; i--)
+        {
+            wrongChain.Add(chain[i]);
+            
+        }
+        
+        for (int i = 0; i < wrongChain.Count; i++)
+        {
+            chain.Remove(wrongChain[i]);
+            prevPos.Remove(wrongChain[i].transform.position);
+           // wrongChain[i].GetComponent<Animator>().enabled = true;
+            //wrongChain[i].GetComponent<Animator>().SetBool("DestroyNucle", true);
+        }
+        target = new Vector3(chain[chain.Count - 1].transform.position.x + 1, chain[chain.Count - 1].transform.position.y, chain[chain.Count - 1].transform.position.z);
+    }
     void MoveNuclei()
     {
         if (chain.Count > 0 && prevPos.Count > 0)
@@ -68,12 +114,34 @@ public class RnaMessengerScript : MonoBehaviour {
             }
         }
     }
+    void MoveBack()
+    {
+        if (chain.Count > 0 && prevPos.Count > 0)
+        {
+            for (int i = 0; i < chain.Count; i++)
+            {
+                if(i == chain.Count -1)
+                  chain[i].transform.position = Vector3.MoveTowards(chain[i].transform.position, target, Time.deltaTime);
+                else
+                {
+                    chain[i].transform.position = Vector3.MoveTowards(chain[i].transform.position, chain[i+1].transform.position, Time.deltaTime);
+                }
+            }
+        }
+    }
     void BreakAnimation()
     {
         if(chain.Count > 1)
         {
-            Destroy(chain[chain.Count - 2].GetComponent<Animator>());
+            chain[chain.Count - 2].GetComponent<Animator>().enabled = false;  
         }
-        
+        if (wrongChain.Count > 0)
+        {
+            for (int i = 0; i < wrongChain.Count; i++)
+            {
+                Destroy(wrongChain[i]);
+            }
+        }
+        wrongChain.Clear();
     }
 }
