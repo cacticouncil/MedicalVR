c47341e1c8fb5f61e812eaa2ba913d128f4c0eed
Jayson Levario <jjlevario@fullsail.edu>
2017-03-09 09:24:47 -0500

    added new scene

---
+++ Medical VR/Assets/DestroyTheCell/Scripts/VirusPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirusPlayer : MonoBehaviour
+{
+    public GameObject TimerText;
+    public float TimeLeft;
+
+	void Start ()
+    {
+        TimeLeft = 60.0f;
+	}
+	
+
+	void Update ()
+    {
+        TimeLeft -= Time.deltaTime;
+        TimerText.GetComponent<TextMesh>().text = "Timer: " + TimeLeft.ToString("f0");
+
+        if (TimeLeft <= 0.0f)
+        {
+            TimeLeft = 0.0f;
+        }
+	}
+}
