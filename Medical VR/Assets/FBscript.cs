7f7edf0c3ef8eface8c985e6ce1a056166fadb7f
Sebastian <sebs_dragneel@hotmail.com>
2017-03-10 11:28:31 -0500

    Facebook Login Done

--- Medical VR/Assets/FBscript.cs
+++ Medical VR/Assets/FBscript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using Facebook.Unity;
@@ -8,8 +9,12 @@ public class FBscript : MonoBehaviour, TimedInputHandler{
 
   public  GameObject DialogLoggedIn;
   public  GameObject DialogLoggedOut;
+  public GameObject DialogUsername;
+  public GameObject DialogProfilePic;
 
-    private void Awake()
+
+
+    void Awake()
     {
         FB.Init(SetInit, OnHideUnity);
     }
@@ -25,14 +30,9 @@ public class FBscript : MonoBehaviour, TimedInputHandler{
     void OnHideUnity(bool IsGameShown)
     {
         if(!IsGameShown)
-        {
             Time.timeScale = 0;
-        }
         else
-        {
             Time.timeScale = 1;
-
-        }
     }
 
     public void HandleTimeInput()
@@ -45,10 +45,7 @@ public class FBscript : MonoBehaviour, TimedInputHandler{
     void AuthCallBack(IResult result)
     {
         if(result.Error != null)
-        {
             Debug.Log(result.Error);
-
-        }
         else
         {
             if (FB.IsLoggedIn)
@@ -66,11 +63,40 @@ public class FBscript : MonoBehaviour, TimedInputHandler{
         {
             DialogLoggedIn.SetActive(true);
             DialogLoggedOut.SetActive(false);
+
+            FB.API("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
+            FB.API("/me/picture?type=square&height=128&width=128", HttpMethod.GET, DisplayProfilePic);
+
         }
         else
         {
             DialogLoggedIn.SetActive(false);
             DialogLoggedOut.SetActive(true);
+        }
+    }
+
+    void DisplayUsername(IResult result)
+    {
+       Text Username = DialogUsername.GetComponent<Text>();
+
+        if(result.Error == null)
+        {
+            Username.text = "Hi there, " + result.ResultDictionary["first_name"];
+        }
+        else
+            Debug.Log(result.Error);
+    }
+
+    void DisplayProfilePic(IGraphResult result)
+    {
+        if (result.Error == null)
+        {
+            Image ProfilePic = DialogProfilePic.GetComponent<Image>();
+            ProfilePic.sprite = Sprite.Create(result.Texture, new Rect(0, 0, 128, 128), new Vector2());
+        }
+        else
+        {
+            Debug.Log(result.Error);
         }
     }
 }
