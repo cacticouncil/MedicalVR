c1d7ea7dfb151d29d3c13e13d856a721e3d10634
Jordan Sanderson <JTSanderson@fullsail.edu>
2017-03-17 16:29:22 -0400

    get ur art here

--- Medical VR/Assets/Rotate.cs
+++ Medical VR/Assets/Rotate.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class Rotate : MonoBehaviour
 {
+	public bool x, y, z;
     public Vector3 rotation;
-	public Color col;
 
     void Update()
-    {
-        transform.Rotate(new Vector3(Time.deltaTime * rotation.x, Time.deltaTime * rotation.y, Time.deltaTime * rotation.z));
-		GetComponent<Renderer> ().material.color = col;
+	{
+		if (x)
+			transform.Rotate(new Vector3(Time.deltaTime * rotation.x, 0, 0));
+		if (y)
+			transform.Rotate(new Vector3(0, Time.deltaTime * rotation.y, 0));
+		if (z)
+			transform.Rotate(new Vector3(0, 0, Time.deltaTime * rotation.z));
     }
 }
