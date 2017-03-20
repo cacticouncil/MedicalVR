a6bea83c78b496a577dc12cb63bfb86805899a25
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-03-20 11:05:02 -0400

    Revert "committing to pull"
    This reverts commit fcb3aad8a6e825c58d606945df4faa8ae92768f6.
    
    # Conflicts:
    #	Medical VR/Assets/Rotate.cs

--- Medical VR/Assets/Rotate.cs
+++ Medical VR/Assets/Rotate.cs
@@ -3,16 +3,12 @@ using System.Collections;
 
 public class Rotate : MonoBehaviour
 {
-	public bool x, y, z;
+    public bool x, y, z;
     public Vector3 rotation;
 
     void Update()
-	{
-		if (x)
-			transform.Rotate(new Vector3(Time.deltaTime * rotation.x, 0, 0));
-		if (y)
-			transform.Rotate(new Vector3(0, Time.deltaTime * rotation.y, 0));
-		if (z)
-			transform.Rotate(new Vector3(0, 0, Time.deltaTime * rotation.z));
+    {
+        transform.Rotate(new Vector3(Time.deltaTime * rotation.x, Time.deltaTime * rotation.y, Time.deltaTime * rotation.z));
+		//GetComponent<Renderer> ().material.color = col;
     }
 }
