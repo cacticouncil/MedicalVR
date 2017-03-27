5bab55791550695d0c0dfa09966c23215d9005db
Michael <Hammeric2000@yahoo.com>
2017-03-27 10:11:12 -0400

    added a nucleus and started working on moving the objects towards the nucleus when they finish combining.

---
+++ Medical VR/Assets/GoogleVR/Scenes/ATPGTPShooter/Scripts/_TEnzymeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _TEnzymeController : MonoBehaviour
+{
+    [HideInInspector]
+    public bool hasATP = false, hasGTP = false;
+
+    public void SetATP()
+    {
+        hasATP = true;
+        UpdateSettings();
+    }
+
+    public void SetGTP()
+    {
+        hasGTP = true;
+        UpdateSettings();
+    }
+    
+    private void UpdateSettings()
+    {
+        if (hasGTP && hasATP)
+            GetComponent<_TTravelToNucleus>().StartTravel();
+    }
+}
---
+++ Medical VR/Assets/GoogleVR/Scenes/ATPGTPShooter/Scripts/_TNucleusController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _TNucleusController : MonoBehaviour
+{
+
+}
