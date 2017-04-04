c73fb015255826037a0b21fc2272144dae6e4945
Jordan Sanderson <JTSanderson@fullsail.edu>
2017-04-04 14:55:22 -0400

    Committing to pull

---
+++ Medical VR/Assets/StrategyTutorial.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrategyTutorial : MonoBehaviour
+{
+    public GameObject redCell;
+    public Vector3 cellPosition;
+
+    private int rNum = 0;
+
+    public void Click()
+    {
+
+    }
+
+    IEnumerator SpawnRedCell()
+    {
+        GameObject c = Instantiate(redCell, cellPosition, redCell.transform.rotation) as GameObject;
+        Vector3 desination;
+        switch (rNum)
+        {
+            case 0:
+                desination = cellPosition;
+                break;
+            default:
+                break;
+        }
+
+        yield return 0;
+    }
+}
