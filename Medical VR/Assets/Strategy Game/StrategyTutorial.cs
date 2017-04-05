5dc95f73e7aa55708faceb26c88db651eb6bf081
Jordan Sanderson <JTSanderson@fullsail.edu>
2017-04-05 09:44:53 -0400

    pull

--- Medical VR/Assets/Strategy Game/StrategyTutorial.cs
+++ Medical VR/Assets/Strategy Game/StrategyTutorial.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class StrategyTutorial : MonoBehaviour
 {
     public GameObject redCell;
+    public GameObject virus;
+    public GameObject whiteCell;
     public Vector3 cellPosition;
     public SimulateSun sun;
     public int cNum = 0;
@@ -11,17 +13,25 @@ public class StrategyTutorial : MonoBehaviour
     public TMPro.TextMeshPro[] text;
 
     private int rNum = 0;
-    private bool spawning = false;
 
     public void Click()
     {
         switch (cNum)
         {
+            case 11:
+                StartCoroutine(TurnTextOn(2));
+
+                break;
             case 4:
+                StartCoroutine(TurnTextOn(1));
+                StartCoroutine(SpawnRedCell());
+                break;
             case 5:
             case 6:
             case 7:
             case 8:
+            case 9:
+            case 10:
                 StartCoroutine(SpawnRedCell());
                 break;
 
@@ -29,11 +39,11 @@ public class StrategyTutorial : MonoBehaviour
                 sun.TurnUpdate();
                 break;
         }
+        cNum++;
     }
 
     IEnumerator SpawnRedCell()
     {
-        spawning = true;
         GameObject c = Instantiate(redCell, cellPosition, redCell.transform.rotation) as GameObject;
         Vector3 desination;
         switch (rNum)
@@ -79,6 +89,38 @@ public class StrategyTutorial : MonoBehaviour
         }
         c.transform.position = desination;
         c.transform.localScale = Vector3.one;
-        spawning = false;
+    }
+
+    IEnumerator TurnTextOn(int index)
+    {
+        Color a = text[index - 1].color;
+        float startTime = Time.time;
+        float percent = 0.0f;
+        while (percent < 1.0f)
+        {
+            percent = (Time.time - startTime) * 2.0f;
+            a.a = Mathf.Lerp(1.0f, 0.0f, percent);
+            text[index - 1].color = a;
+            yield return 0;
+        }
+        a.a = 0.0f;
+        text[index - 1].color = a;
+        yield return 0;
+        text[index - 1].gameObject.SetActive(false);
+        text[index].gameObject.SetActive(true);
+        a = text[index].color;
+        a.a = 0.0f;
+        text[index].color = a;
+        startTime = Time.time;
+        percent = (Time.time - startTime);
+        while (percent < 1.0f)
+        {
+            percent = (Time.time - startTime) * 2.0f;
+            a.a = Mathf.Lerp(0.0f, 1.0f, percent);
+            text[index].color = a;
+            yield return 0;
+        }
+        a.a = 1.0f;
+        text[index].color = a;
     }
 }
