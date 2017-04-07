d3f071c3bab0f7f8615de71c77131d173aa4299e
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-04-07 15:24:34 -0400

    Virus Gameplay Scene.unity is now playable. Need to add some other visual events, populate some scenes, etc.

---
+++ Medical VR/Assets/Virus Gameplay/Rna_VirusGameplayScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Rna_VirusGameplayScript : MonoBehaviour {
+
+    public List<GameObject> places;
+    public GameObject subtitltes;
+
+    delegate void Func();
+    Func doAction;
+    public float moveSpeed;
+    float fadeSpeed;
+    int I = 0;
+    void Start()
+    {
+        gameObject.SetActive(false);
+        doAction = NullFunction;
+        switch (VirusGameplayScript.loadCase)
+        {
+            
+            default:
+                break;
+        }
+    }
+    void NullFunction()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CheckCaases();
+        doAction();
+        MoveTo();
+    }
+    void MoveTo()
+    {
+       if(I != places.Count) 
+       transform.position = Vector3.MoveTowards(transform.position, places[I].transform.position, moveSpeed * Time.deltaTime);
+    }
+       
+    void CheckCaases()
+    {
+        switch ((int)subtitltes.GetComponent<SubstitlesScript>().theTimer)
+        {
+            case (288):
+                
+                break;
+            case (296):
+                I = 1;
+                transform.position = places[I].transform.position;
+                break;
+            case (297):
+                I = 2;
+                moveSpeed = 175;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
