e0bf4537fe9cce07c570e4c741029866a534d8a5
Michael <Hammeric2000@yahoo.com>
2017-04-10 15:43:07 -0400

    modified the UI, making a set up so objects appear in the GUI.

---
+++ Medical VR/Assets/GoogleVR/Scenes/ATPGTPShooter/Scripts/_TCreateObjects.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _TCreateObjects : MonoBehaviour {
+    GameObject parent;
+    GameObject objects;
+    int numberOfObjects;
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < numberOfObjects; ++i)
+            Instantiate(objects, parent.transform);
+    }
+}
