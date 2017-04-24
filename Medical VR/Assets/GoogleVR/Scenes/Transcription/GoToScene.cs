77bdd8be1d6aae91e3361ebd31879e331e9f3891
Sebastian <sebs_dragneel@hotmail.com>
2017-04-24 11:01:13 -0400

    fixed some stuff

--- Medical VR/Assets/GoogleVR/Scenes/Transcription/GoToScene.cs
+++ Medical VR/Assets/GoogleVR/Scenes/Transcription/GoToScene.cs
@@ -6,14 +6,23 @@ using UnityEngine.SceneManagement;
 public class GoToScene : MonoBehaviour {
    public void GoToStoryMemory()
     {
+        
+        if(MemoryUI.arcadeMode == false)
         MemoryUI.arcadeMode = false;
+        else
+        MemoryUI.arcadeMode = true;
+
         MemoryUI.TutorialMode = false;
         SceneManager.LoadScene("MemoryGame");
     }
 
    public void GoToStoryCGampSnatcher()
     {
+        if(Storebullets.arcadeMode == false)
         Storebullets.arcadeMode = false;
+        else
+            Storebullets.arcadeMode = true;
+
         Storebullets.TutorialMode = false;
         SceneManager.LoadScene("CGampSnatcher");
     }
