39b15ca888980db0e49d9d54d4fa6779cb65e67d
Jayson Levario <jjlevario@fullsail.edu>
2017-04-25 16:02:19 -0400

    added broken pause

---
+++ Medical VR/Assets/UnpauseScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnpauseScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Player;
+    public GameObject Pause;
+
+    public void UnpauseGame()
+    {
+        MainMenu.SetActive(false);
+        Pause.GetComponent<Pause>().isPaused = false;
+        Time.timeScale = 1;
+    }
+}
