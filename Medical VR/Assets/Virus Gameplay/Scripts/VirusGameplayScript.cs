fb26898ccc687f0c9f2272cf4f847608b62f4b65
Jordan Sanderson <JTSanderson@fullsail.edu>
2017-04-29 12:30:52 -0400

    fixed glitches

--- Medical VR/Assets/Virus Gameplay/Scripts/VirusGameplayScript.cs
+++ Medical VR/Assets/Virus Gameplay/Scripts/VirusGameplayScript.cs
@@ -8,7 +8,7 @@ public class VirusGameplayScript : MonoBehaviour
     public List<GameObject> places;
     public List<GameObject> Sceneries;
     public GameObject subtitles, blackCurtain, theCamera, virus, rna;
-    public static int loadCase = 2;
+    public static int loadCase;
     // Use this for initialization
     delegate void Func();
     Func doAction;
