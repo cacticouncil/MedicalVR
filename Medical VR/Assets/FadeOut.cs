d00109d68ff4883f89fc7f253c73f8e7b2a55143
Jordan Sanderson <JTSanderson@fullsail.edu>
2017-05-09 14:55:47 -0400

    No way, this is for Horse-ay

---
+++ Medical VR/Assets/FadeOut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(FadeOutObject(gameObject.GetComponent<Renderer>()));
+    }
+
+    IEnumerator FadeOutObject(Renderer g)
+    {
+        if (g.material.HasProperty("_Color"))
+        {
+            Color c = g.material.color;
+            c.a = 1.0f;
+            float startTime = Time.time;
+            float t = 0.0f;
+            while (t < 1)
+            {
+                t = Time.time - startTime;
+                c.a = 1.0f - t;
+                g.material.color = c;
+                yield return 0;
+            }
+            c.a = 1.0f;
+            g.material.color = c;
+        }
+        Destroy(gameObject);
+    }
+}
