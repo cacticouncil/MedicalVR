41d460342eeaa140e1724ec8c5da20ba731e7c3d
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2017-05-11 09:35:08 -0400

    committing to pull

---
+++ Medical VR/Assets/Virus Gameplay/Scripts/GotoNucleus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GotoNucleus : MonoBehaviour {
+
+    public GameObject Manager;
+    bool doStuff = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		if(doStuff == true && (int)Manager.GetComponent<VirusGameplayScript>().subtitles.GetComponent<SubstitlesScript>().theTimer == 249)
+        {
+            Manager.GetComponent<VirusGameplayScript>().LowerCurtain();
+            if(Manager.GetComponent<VirusGameplayScript>().blackCurtain.GetComponent<Renderer>().material.color.a >=1)
+            {
+                Manager.GetComponent<VirusGameplayScript>().GoToNucleus();
+                this.gameObject.SetActive(false);
+            }
+        }
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            doStuff = true;
+        }
+    }
+}
