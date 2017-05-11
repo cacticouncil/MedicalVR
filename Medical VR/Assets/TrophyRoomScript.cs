cd79dd5feac29f2034056c21666fb12f5d6a5ca9
Jayson Levario <jjlevario@fullsail.edu>
2017-05-11 09:36:36 -0400

    started a new script

---
+++ Medical VR/Assets/TrophyRoomScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TrophyRoomScript : MonoBehaviour
+{
+    public GameObject Text;
+    public GameObject TrophyStand1;
+    public GameObject TrophyStand2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator DisplayText(string message, float duration)
+    {
+        Text.GetComponent<TextMeshPro>().enabled = true;
+        Text.GetComponent<TextMeshPro>().text = message;
+        yield return new WaitForSeconds(duration);
+        Text.GetComponent<TextMeshPro>().enabled = false;
+    }
+}
