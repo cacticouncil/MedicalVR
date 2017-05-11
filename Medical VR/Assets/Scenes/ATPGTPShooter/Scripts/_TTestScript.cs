2fc6da89903a8b2e693c964143cae1c18f67adce
Michael <Hammeric2000@yahoo.com>
2017-05-11 09:44:11 -0400

    updated the atpgtp tutorial.

--- Medical VR/Assets/Scenes/ATPGTPShooter/Scripts/_TTestScript.cs
+++ Medical VR/Assets/Scenes/ATPGTPShooter/Scripts/_TTestScript.cs
@@ -12,3 +12,27 @@ public class _TTestScript : MonoBehaviour
     }
 
 }
+
+//public class _TTestScript : MonoBehaviour
+//{
+//    Rigidbody rb;
+//
+//    public float timeMultiplier;
+//    private void Start()
+//    {
+//        rb = GetComponent<Rigidbody>();
+//        transform.rotation = Random.rotation;
+//
+//    }
+//
+//    private void FixedUpdate()
+//    {
+//
+//        float min = 0.3f;
+//        float max = 0.7f;
+//        rb.angularVelocity = Random.insideUnitSphere * timeMultiplier;
+//        // transform.Rotate(Random.Range(min, max) * timeMultiplier, Random.Range(min, max) * timeMultiplier, Random.Range(min, max) * timeMultiplier);
+//        //transform.Rotate(Vector3.right * Time.deltaTime);
+//    }
+//
+//}
