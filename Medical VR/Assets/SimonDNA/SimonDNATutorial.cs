848f3ad070ffb314fb015bcdd9aec7ea5d0019d9
Jayson Levario <jjlevario@fullsail.edu>
2017-05-17 16:34:46 -0400

    refactored the fight virus game

--- Medical VR/Assets/SimonDNA/SimonDNATutorial.cs
+++ Medical VR/Assets/SimonDNA/SimonDNATutorial.cs
@@ -25,24 +25,25 @@ public class SimonDNATutorial : MonoBehaviour
 
     void Start()
     {
-        //if (GlobalVariables.tutorial == true)
-        //{
-        //    prevState = GlobalVariables.arcadeMode;
-        //    GlobalVariables.arcadeMode = false;
-        //    Subtitles.SetActive(true);
-        //    Player.GetComponent<SimonSays>().GO.SetActive(false);
-        //    UI.SetActive(false);
+        if (GlobalVariables.tutorial == true)
+        {
+            prevState = GlobalVariables.arcadeMode;
+            GlobalVariables.arcadeMode = false;
+            Subtitles.SetActive(true);
+            Player.GetComponent<SimonSays>().GO.SetActive(false);
+            UI.SetActive(false);
 
-        //    TextList[0] = "Listen closely human, here is your objective. ";
-        //    TextList[1] = "Nucleotides will pop up on the screen in a certain order.";
-        //    TextList[2] = "You will always have four nucleotides around you which represent all the types of nucleotides that could pop up.";
-        //    TextList[3] = "Once the GO! sign appears, use the four nucleotides to press them in the same order that the previous nucleotides showed up.";
-        //    TextList[4] = "Get it correct to build up the messenger RNA, getting it incorrect will cancel out the part you are building.";
-        //    TextList[5] = "Build a RNA Messenger witht the amount of nucleotides displayed on the goal to complete the messeger, Good Luck!.";
-        //    TextForTutorial();
-        //}
-        //else
-        enabled = false;
+            TextList[0] = "Listen closely human, here is your objective. ";
+            TextList[1] = "Nucleotides will pop up on the screen in a certain order.";
+            TextList[2] = "You will always have four nucleotides around you which represent all the types of nucleotides that could pop up.";
+            TextList[3] = "Once the GO! sign appears, use the four nucleotides to press them in the same order that the previous nucleotides showed up.";
+            TextList[4] = "Get it correct to build up the messenger RNA, getting it incorrect will cancel out the part you are building.";
+            TextList[5] = "Build a RNA Messenger witht the amount of nucleotides displayed on the goal to complete the messeger, Good Luck!.";
+            TextForTutorial();
+        }
+
+        else
+            enabled = false;
     }
 
     void Update()
