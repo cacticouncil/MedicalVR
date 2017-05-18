6f8d2370af0b1d7335e40e5dfb0d219c4341ab4b
Sebastian <sebs_dragneel@hotmail.com>
2017-05-18 09:32:12 -0400

    more Credits.unity

--- Medical VR/Assets/Credits/VesselOpeningCredits.cs
+++ Medical VR/Assets/Credits/VesselOpeningCredits.cs
@@ -4,19 +4,10 @@ using System.Collections;
 public class VesselOpeningCredits : MonoBehaviour
 {
     public Transform otherSide = null;
-    public bool shouldCollide;
-    // Use this for initialization
-    void Start()
-    {
-        shouldCollide = true;
-    }
+
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            shouldCollide = !shouldCollide;
-        }
-        else if (otherSide != null)
+        if (otherSide != null)
         {
             Vector3 diff = transform.position - otherSide.position;
             diff *= .9f;
